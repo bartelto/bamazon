40d2e645d79938575773e5354f4fa6cab897e4e6
Miles Standoffish <toddnan@gmail.com>
2019-08-17 23:27:30 -0500

    SQL schema and seed files

---
+++ seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 
+('Frisbee', 'Toys', 4.25, 17),
+('Water Bottle', 'Travel', 9.75, 23),
+('Printer Paper', 'Office', 11.55, 53),
+('Security Envelopes', 'Office', 6.25, 7),
+('Fingernail Clippers', 'Personal Care', 2.99, 9),
+('Monkey Puppet', 'Toys', 23.05, 12)
