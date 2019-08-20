82242855ab585b81c7d9ead922f5fe30a6946282
Miles Standoffish <toddnan@gmail.com>
2019-08-20 12:34:56 -0500

    purchase functionality working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,7 +12,7 @@ let connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("Bamazon connected as id " + connection.threadId + "\n");
+    console.log("Welcome to Bamazon!".yellow + "\nConnected as id " + connection.threadId + "\n");
     showProducts();
 });
 
@@ -37,16 +37,60 @@ function showProducts() {
                 {
                     name: "qty",
                     message: "Enter the quantity:",
-                    type: "number"
+                    type: "number",
+                    validate: function(val) {
+                        if (val < 1) {
+                            return "Please enter a positive quantity.";
+                        } else return true;
+                    }
                 }
             ]).then(function(response) {
-                
-                    
-            
+                // check if the store has sufficient stock
+                connection.query(
+                    "SELECT * FROM products WHERE item_id = ?", [response.id],
+                    function(err, res) {
+                        if (err) throw err;
+                        //console.log(res);
+                        if (res[0].stock_quantity < response.qty) {
+                            if (res[0].stock_quantity <= 0) {
+                                console.log(`\nSorry, but that item is out of stock.\n`.yellow);
+                            } else {
+                                console.log(`\nSorry, but there are only ${res[0].stock_quantity.toString().yellow} items available.\n`);
+                            }
+                            continueOrExit();
+                        } else { // sufficient qty in stock
+                            connection.query(
+                                "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?", [response.qty, response.id],
+                                function(err) {
+                                    if (err) throw err;
+                                    console.log("\nThank you for your purchase!".green + 
+                                        `\nItem ${res[0].item_id} - ${res[0].product_name} - qty. ${response.qty}` +
+                                        `\nUnit cost:  $ ${res[0].price.toFixed(2).padStart(6)}` +
+                                        `\nTotal cost: $ ${(response.qty * res[0].price).toFixed(2).padStart(6)}\n`
+                                    );
+                                    continueOrExit();
+                                }
+                            );
+                        }
+                    }
+                );
             });
-
-            connection.end();
         }
     );
 }
 
+function continueOrExit() {
+    inquirer.prompt(
+        {
+            name: "continue",
+            message: "Continue shopping? Press enter to continue, or type 'n' to exit.)",
+            type: "confirm"
+        }
+    ).then(function(response) {
+        if (response.continue) { 
+            console.log("\n"); 
+            showProducts(); 
+        }
+        else connection.end();
+    });
+}
