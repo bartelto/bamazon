93c878acabd5fbb7bb28798a9be6c75925506af0
Miles Standoffish <toddnan@gmail.com>
2019-08-21 10:40:45 -0500

    manager app complete

--- manager/bamazonManager.js
+++ manager/bamazonManager.js
@@ -31,22 +31,19 @@ function showOptions() {
         switch (response.command) {
            
             case "View Products for Sale":
-                
+                viewProducts();
                 break;
-        
             
             case "View Low Inventory":
-                
+                viewLowInventory()
                 break;
-
                
             case "Add to Inventory":
-                
+                addToInventory();
                 break;
-
         
             case "Add New Product":
-                
+                addNewProduct();
                 break;
 
             // Exit 
@@ -61,4 +58,107 @@ function showOptions() {
         }
     });
     
+}
+
+function viewProducts() {
+    connection.query(
+        "SELECT * FROM products ORDER BY department_name",
+        function(err, res) {
+            if (err) throw err;
+            console.log(`\n${"ID".padEnd(2,' ')}  ${"PRODUCT NAME".padEnd(25,' ')} ${"DEPARTMENT".padEnd(20,' ')}   ${"PRICE".padStart(6,' ')} ${"QTY".padStart(8,' ')}`.blue);
+            console.log("".padEnd(68,"-").bold);
+            res.forEach(function(row){
+                console.log(`${row.item_id.toString().padStart(2,' ')}  ${row.product_name.padEnd(25,' ')} ${row.department_name.padEnd(20,' ')} $ ${row.price.toFixed(2).padStart(6,' ')} ${row.stock_quantity.toString().padStart(8,' ')}`);
+            });
+            console.log('\n');
+
+            showOptions();
+        }
+    );
+    
+}
+
+function viewLowInventory() {
+    connection.query(
+        "SELECT * FROM products WHERE stock_quantity < 5 ORDER BY department_name",
+        function(err, res) {
+            if (err) throw err;
+            console.log(`\n${"ID".padEnd(2,' ')}  ${"PRODUCT NAME".padEnd(25,' ')} ${"DEPARTMENT".padEnd(20,' ')}   ${"PRICE".padStart(6,' ')} ${"QTY".padStart(8,' ')}`.blue);
+            console.log("".padEnd(68,"-").bold);
+            res.forEach(function(row){
+                console.log(`${row.item_id.toString().padStart(2,' ')}  ${row.product_name.padEnd(25,' ')} ${row.department_name.padEnd(20,' ')} $ ${row.price.toFixed(2).padStart(6,' ')} ${row.stock_quantity.toString().padStart(8,' ')}`);
+            });
+            console.log('\n');
+            
+            showOptions();
+        }
+    );
+    
+}
+
+function addToInventory() {
+    inquirer.prompt([
+        {
+            type: 'number',
+            name: "id",
+            message: "Enter the product ID to which inventory is being added:"
+        },
+        {
+            type: 'number',
+            name: "qty",
+            message: "Enter the quantity being added:"
+        }
+    ]).then(function(response) {
+        connection.query(
+            "UPDATE products SET stock_quantity = stock_quantity + ? WHERE item_id = ?",
+            [response.qty, response.id],
+            function(err) {
+                if (err) throw err;
+                console.log(`Quantity ${response.qty} added to product ${response.id}.`);
+                console.log('\n');
+                
+                showOptions();
+            }
+        );
+    });
+}
+
+function addNewProduct() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: "name",
+            message: "Enter the name of the new product:"
+        },
+        {
+            type: 'input',
+            name: "dept",
+            message: "Enter the department:"
+        },
+        {
+            type: 'number',
+            name: "price",
+            message: "Enter the unit price:"
+        },
+        {
+            type: 'number',
+            name: "qty",
+            message: "Enter the initial stock quantity:"
+        }
+    ]).then(function(response) {
+        connection.query(
+            "INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (?,?,?,?)",
+            [response.name, response.dept, response.price, response.qty],
+            function(err) {
+                if (err) throw err;
+                console.log("\nProduct added:\n".green);
+                console.log(`${"PRODUCT NAME".padEnd(25,' ')} ${"DEPARTMENT".padEnd(20,' ')}   ${"PRICE".padStart(6,' ')} ${"QTY".padStart(8,' ')}`.blue);
+                console.log("".padEnd(68,"-").bold);
+                console.log(`${response.name.padEnd(25,' ')} ${response.dept.padEnd(20,' ')} $ ${response.price.toFixed(2).padStart(6,' ')} ${response.qty.toString().padStart(8,' ')}`);
+                console.log('\n')
+                
+                showOptions();
+            }
+        );
+    });
 }
